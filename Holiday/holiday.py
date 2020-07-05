8132a0d74c75eeb2e93f895274fbaa3d1550bba3
Joey Chen <jay.hatelove@gmail.com>
2020-07-05 14:48:04 +0800

    [holiday]pass test by mock.patch() to stub today value

--- Holiday/holiday.py
+++ Holiday/holiday.py
@@ -2,7 +2,11 @@ from datetime import date
 
 
 def say_hello():
-    today = date.today()
+    today = __get_today()
     if today.strftime("%m/%d") == "12/25":
         return "Merry Xmas"
     return "Today is not Xmas"
+
+
+def __get_today():
+    return date.today()
--- Holiday/holiday_tests.py
+++ Holiday/holiday_tests.py
@@ -1,11 +1,21 @@
 import unittest
+from datetime import date
+from unittest.mock import patch
 
 from Holiday.holiday import say_hello
 
 
 class HolidayTests(unittest.TestCase):
     def test_today_is_xmas(self):
+        self.fake_get_today.return_value = date(2020, 12, 25)
         self.assertEqual("Merry Xmas", say_hello())
+
+    def setUp(self) -> None:
+        get_today_patcher = patch('Holiday.holiday.__get_today')
+        self.fake_get_today = get_today_patcher.start()
+
+    def tearDown(self) -> None:
+        patch.stopall()
 
 
 if __name__ == '__main__':
