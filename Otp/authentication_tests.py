8bb94e3678a2aeb4d9671ad5854be03a7164dc90
Joey Chen <jay.hatelove@gmail.com>
2020-07-05 15:45:15 +0800

    [otp]refactor test: "is_valid"

--- Otp/authentication_tests.py
+++ Otp/authentication_tests.py
@@ -6,9 +6,18 @@ from Otp.authentication_service import is_valid
 
 class AuthenticationServiceTests(unittest.TestCase):
     def test_is_valid(self):
-        self.fake_get_password.return_value = "91"
-        self.fake_get_otp.return_value = "000000"
-        self.assertEqual(True, is_valid("joey", "91000000"))
+        self.given_password("91")
+        self.given_otp("000000")
+        self.should_be_valid("joey", "91000000")
+
+    def should_be_valid(self, account, password):
+        self.assertEqual(True, is_valid(account, password))
+
+    def given_otp(self, otp):
+        self.fake_get_otp.return_value = otp
+
+    def given_password(self, password):
+        self.fake_get_password.return_value = password
 
     def setUp(self) -> None:
         get_password_patcher = patch('Otp.authentication_service.get_password_from_db')
