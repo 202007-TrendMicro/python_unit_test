c6cdffa36d1e8db815b4bf4fc7f35cba3bcedcdc
Joey Chen <jay.hatelove@gmail.com>
2020-07-05 16:30:41 +0800

    [order]refactor test

--- Order/order_service_tests.py
+++ Order/order_service_tests.py
@@ -8,6 +8,7 @@ from Order.order_service import OrderService, Order
 
 class OrderServiceTests(unittest.TestCase):
     def setUp(self) -> None:
+        self.order_service = OrderService()
         get_orders_patcher = patch('Order.order_service.OrderService.get_orders')
         self.fake_get_orders = get_orders_patcher.start()
         insert_patcher = patch('Order.order_service.BookDao.insert')
@@ -17,20 +18,34 @@ class OrderServiceTests(unittest.TestCase):
         patch.stopall()
 
     def test_only_2_book_orders_in_3_orders_when_sync_book_orders(self):
-        self.fake_get_orders.return_value = [
+        self.given_orders([
             Order(order_type="Book", price=100),
             Order(order_type="CD"),
             Order(order_type="Book", price=200),
-        ]
+        ])
 
-        order_service = OrderService()
-        order_service.sync_book_orders()
+        self.when_sync_book_order()
 
-        self.assertEqual(2, self.fake_insert.call_count)
-        first_order = self.fake_insert.call_args_list[0].args[0]
-        compare(Order(order_type="Book", price=100), first_order)
+        self.should_insert_order_count(2)
+        self.first_inserted_order_should_be(Order(order_type="Book", price=100))
+        self.second_inserted_order_should_be(Order(order_type="Book", price=200))
+
+    def second_inserted_order_should_be(self, expected):
         second_order = self.fake_insert.call_args_list[1].args[0]
-        compare(Order(order_type="Book", price=200), second_order)
+        compare(expected, second_order)
+
+    def first_inserted_order_should_be(self, expected):
+        first_order = self.fake_insert.call_args_list[0].args[0]
+        compare(expected, first_order)
+
+    def should_insert_order_count(self, times):
+        self.assertEqual(times, self.fake_insert.call_count)
+
+    def when_sync_book_order(self):
+        self.order_service.sync_book_orders()
+
+    def given_orders(self, orders):
+        self.fake_get_orders.return_value = orders
 
 
 if __name__ == '__main__':
